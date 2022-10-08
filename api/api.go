d9be6b79393a88e439a109ab0fcbe73e6d2a6fe2
Luciano Ribeiro <luciano.ribeiro@outlook.com>
2022-10-08 13:04:49 -0300

    Implementando testes e funções.

--- api/api.go
+++
@@ -1,3 +0,0 @@
-// check if a string is valid, if it finds 2 or more identical four-letter strings in any direction
-
-
--- main.go
+++ main.go
@@ -2,14 +2,37 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+
+	"github.com/lucianorbr/teste_Capgemini/controllers"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World")
-	})
-	http.ListenAndServe(":8080", nil)
+var letters = []string{
+	"DUHBHB",
+	"DUBUHD",
+	"UBUUHU",
+	"BHBDHH",
+	"DDDDUB",
+	"UDBDUH",
+	"NNNNNN",
+	"MMMMMN",
+	"NNNNNN",
+	"HHHHHH",
+	"YYYYYY",
+	"XXXXXX",
 }
 
+var resultisvalid = controllers.CountIsValid(letters)
+var resultratio = controllers.CountRatio(letters)
+var resultinvalid = controllers.CountInValid(letters)
 
+func main() {
+
+	for i := 0; i < len(letters); i++ {
+		fmt.Printf("{\"is_valid\": %v}\n", controllers.IsValid(letters[i]))
+	}
+
+	fmt.Printf("{\"count_valid\": %d}\n", resultisvalid)
+	fmt.Printf("{\"count_invalid\": %d}\n", resultinvalid)
+	fmt.Printf("{\"ratio\": %.1f}\n", resultratio)
+
+}
